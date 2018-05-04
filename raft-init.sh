1f5dba7ea2231f4c9eb9d20d766bc7bafe3538b0
vasa <vasa.develop@gmail.com>
2018-05-04 12:58:16 +0530

    Update raft-init.sh

--- raft-init.sh
+++ raft-init.sh
@@ -8,8 +8,7 @@ mkdir -p qdata/logs
 
 echo "[*] Configuring node"
 mkdir -p qdata/{keystore,geth}
-# cp raft/static-nodes.json qdata
-# cp raft/keystore/$(ls raft/keystore) qdata/keystore/acckey
+cp raft/static-nodes.json qdata
 cp keys/key1.json qdata/keystore/acckey
 cp raft/geth/nodekey1 qdata/geth/nodekey
 geth --datadir qdata init genesis.json
