92bd79444ba941291aab2ae404fcc3751c88f024
vasa-develop <vasa.develop@gmail.com>
2018-05-04 11:58:26 +0530

    initial commit

---
+++ raft-init.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -u
+set -e
+
+echo "[*] Cleaning up temporary data directories"
+rm -rf qdata
+mkdir -p qdata/logs
+
+echo "[*] Configuring node"
+mkdir -p qdata/{keystore,geth}
+# cp raft/static-nodes.json qdata
+# cp raft/keystore/$(ls raft/keystore) qdata/keystore/acckey
+cp keys/key1.json qdata/keystore/acckey
+cp raft/geth/nodekey1 qdata/geth/nodekey
+geth --datadir qdata init genesis.json
